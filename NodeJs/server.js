92e89edf5d14e0e4a71498e3826936213ba0ebd2
Sthefany <sthefany.cedeno@est.fi.uncoma.edu.ar>
2024-11-04 11:37:10 -0300

    hice dos endpoints

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -4,10 +4,29 @@
 // npm install express
 
 //importa el express
-const express = require('express');
+const express = require("express");
+
+const bodyParser = require("body-parser");
 
 //instancia express
 const app = express();
+
+const readData = () => {
+  try {
+    const data = fs.readFileSync("./bdComics.json");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const writeData = (data) => {
+  try {
+    fs.writeFileSync("./bdComics.json", JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 /*DUDAS IP*/
 /*
@@ -22,56 +41,83 @@ la ip ahi sera lo que esta en INET, y copiar hasta al ultimo digito que dps hay 
 por ejemplo  inet 111.11.111.111/16, solo copias 111.11.111.111 
 */
 
-const ip = '172.16.100.172';
+const ip = "192.168.0.8";
 // const ip = 'localhost';
 
 //puede ser cualquier puerto pero que sea superior a 1024, 3000 es buena opcion
 //y la mas usado cuando se aprende.
-const puerto = '3030';
+const puerto = "3030";
 
 //importamos path que otorga direcciones compaible para cualquier OS
-const path = require('path');
+const path = require("path");
 
 // usaremos el file system nomas para verificar si existe algun elemento
 // es una cuestion para ver si cometimos typos o alguna ruta incorrecta ;)
-const fs = require('fs');
+const fs = require("fs");
 
 // con el path, creamos la direccion donde se encuentra nuestro proyecto
-const staticPath = path.join(__dirname, '../Proyecto-Tienda');
+const staticPath = path.join(__dirname, "../Proyecto-Tienda");
 
 //imprimimos para observar si es la ruta que deseamos usar
-console.log('Ruta del proyectos:', staticPath);
+console.log("Ruta del proyectos:", staticPath);
 
 // con fs preguntamos si la ruta existe, de no ser asi, finalizamos el codigo
 if (!fs.existsSync(staticPath)) {
-    console.error('Che cap@, me diste una ruta inexistente:', staticPath);
-    process.exit(1);
+  console.error("Che cap@, me diste una ruta inexistente:", staticPath);
+  process.exit(1);
 } else {
-    console.log('alfin, tanto te costo darme la ruta buena?');
+  console.log("alfin, tanto te costo darme la ruta buena?");
 }
 
 // asignamos la direccion del index
-const indexPath = path.join(staticPath, '/HTML/index.html');
+const indexPath = path.join(staticPath, "/HTML/index.html");
 
 // y con fs probamos si existe
 if (!fs.existsSync(indexPath)) {
-    console.error('todo bien con la ruta, pero y el archivo? mira lo que me pasaste:', indexPath);
-    process.exit(1);
-}else {
-    console.log('vamaaah bien ahi, el index funcaaaaaa');
+  console.error(
+    "todo bien con la ruta, pero y el archivo? mira lo que me pasaste:",
+    indexPath
+  );
+  process.exit(1);
+} else {
+  console.log("vamaaah bien ahi, el index funcaaaaaa");
 }
 
 // agregamos al servidor la ruta del proyecto
 app.use(express.static(staticPath));
 
 // cuando el usuario entre al server, se lo llevara al index
-app.get('/', (req, res) => { // "/" indica la raiz
-    res.sendFile(indexPath);
+app.get("/", (req, res) => {
+  // "/" indica la raiz
+  res.sendFile(indexPath);
 });
 
 // abrimos el server en el puerto que especificamos
 app.listen(puerto, () => {
-    console.log('mucho tramite, pero finalmente aca esta el link: http://' + ip + ':' + puerto);
+  console.log(
+    "mucho tramite, pero finalmente aca esta el link: http://" +
+      ip +
+      ":" +
+      puerto
+  );
 });
 // luego con npm start deberias poder abrir el servidor
 // tambien con node server.js (si asi llama este archivo)
+
+//Endponit para buscar todos los recursos
+app.get("/api/comics", (req, res) => {
+  const data = readData();
+  res.json(data.comics);
+});
+
+//Endpoint para buscar un  comic por su id sino lo encuetra devuelve 404
+app.get("/api/comics/:id", (req, res) => {
+  const data = readData();
+  const id = parseInt(req.params.id);
+  const comic = data.comics.find((comic) => comic.id === id);
+  if (!comic) {
+    // Si el cómic no se encuentra, respondemos con un 404 y un mensaje de error.
+    return res.status(404).json({ error: "Cómic no encontrado" });
+  }
+  res.json(comic);
+});
